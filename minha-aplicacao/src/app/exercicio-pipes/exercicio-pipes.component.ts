6c1815b215b3f8707b9c038e7ce0eeea731df2f0
Thiago Aquino <aquinolima.thiago@gmail.com>
2021-02-21 02:41:01 -0300

    Desafio Pipes

--- minha-aplicacao/src/app/exercicio-pipes/exercicio-pipes.component.ts
+++ minha-aplicacao/src/app/exercicio-pipes/exercicio-pipes.component.ts
@@ -19,4 +19,18 @@ export class ExercicioPipesComponent implements OnInit {
     dataLancamento: new Date(2019, 12, 30),    
   };
 
+  compras = [{
+    produto: 'lâmpadas',
+    valor: 299.29,
+    quantidade: 2,
+    peso: 0,
+    data: new Date(2020, 1, 1, 15, 20),
+}, {
+    produto: 'farinha',
+    valor: 450.29,
+    quantidade: 2,
+    peso: 29.33333,
+    data: new Date(2020, 1, 10, 19, 30),
+}];
+
 }
