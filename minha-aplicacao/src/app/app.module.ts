afbf426118f71df1d28028bac285db5eff9c565a
Thiago Aquino <aquinolima.thiago@gmail.com>
2021-02-21 03:05:51 -0300

    rearranging the project

--- minha-aplicacao/src/app/app.module.ts
+++ minha-aplicacao/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { ExercicioDataBindingComponent } from './exercicio-data-binding/exercicio-data-binding.component';
-import { ContadorComponent } from './exercicio-data-binding/contador/contador.component';
-import { ExerciciosDiretivasComponent } from './exercicios-diretivas/exercicios-diretivas.component';
-import { ExercicioNgclassComponent } from './exercicio-ngclass/exercicio-ngclass.component';
-import { ExercicioPipesComponent } from './exercicio-pipes/exercicio-pipes.component';
+import { ExercicioDataBindingComponent } from './shared/exercicios/exercicio-data-binding/exercicio-data-binding.component';
+import { ContadorComponent } from './shared/exercicios/contador/contador.component';
+import { ExerciciosDiretivasComponent } from './shared/exercicios/exercicios-diretivas/exercicios-diretivas.component';
+import { ExercicioNgclassComponent } from './shared/exercicios/exercicio-ngclass/exercicio-ngclass.component';
+import { ExercicioPipesComponent } from './shared/exercicios/exercicio-pipes/exercicio-pipes.component';
 
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
