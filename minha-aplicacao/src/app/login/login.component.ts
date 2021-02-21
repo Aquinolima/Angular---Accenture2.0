f4817b4bf833368b397f374f0102cb8ed067ab95
Thiago Aquino <aquinolima.thiago@gmail.com>
2021-02-21 19:11:13 -0300

    formulario de login template driven

--- minha-aplicacao/src/app/login/login.component.ts
+++ minha-aplicacao/src/app/login/login.component.ts
@@ -1,25 +1,49 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+
+  @ViewChild('emailInput')
+  emailInput!: ElementRef;
+  @ViewChild('passwordInput')
+  passwordInput!: ElementRef;
 
   email!: string;
   password!: string;
 
-  constructor() { }
+  onSubmit(form: any) {
+    if (!form.valid) {
+      form.controls.email.markAsTouched();
+      form.controls.password.markAsTouched();
 
-  ngOnInit(): void {
+      if (form.controls.email.invalid) {
+        this.emailInput.nativeElement.focus();
+        return;
+      }
+
+      if (form.controls.password.invalid) {
+        this.passwordInput.nativeElement.focus();
+        return;
+      }
+
+      return;
+    }
+
+    console.log('email: ', this.email);
+    console.log('password: ', this.password);
   }
 
-  onSubmit(form: any){
-    console.log(form.value);
-    console.log('Email: ', this.email);
-    console.log('Password: ', this.password);
+  exibeErro(nomeControle: string, form: NgForm) {
+    if (!form.controls[nomeControle]) {
+      return false;
+    }
+
+    return form.controls[nomeControle].invalid && form.controls[nomeControle].touched;
   }
 
 }
