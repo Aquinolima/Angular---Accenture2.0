d1f2ac4279beead538066216262255fd14222f0a
Thiago Aquino <aquinolima.thiago@gmail.com>
2021-02-26 02:53:16 -0300

    Contact Delete

--- minha-aplicacao/src/app/app.module.ts
+++ minha-aplicacao/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 import { SharedModule } from './shared/shared.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ToastrModule } from 'ngx-toastr';
 
 
 registerLocaleData(localePt, 'pt');
@@ -24,6 +26,8 @@ registerLocaleData(localePt, 'pt');
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [{
     provide: LOCALE_ID,
