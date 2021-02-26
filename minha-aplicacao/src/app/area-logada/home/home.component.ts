267db5bed2a0258696ae84455b91a39e9ca6ad03
Thiago Aquino <aquinolima.thiago@gmail.com>
2021-02-26 01:16:52 -0300

    Rotas Filhas (Componente de Área Logada)

--- minha-aplicacao/src/app/area-logada/home/home.component.ts
+++ minha-aplicacao/src/app/area-logada/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Usuario } from '../shared/interfaces/usuario.interface';
-import { AuthService } from '../shared/services/auth/auth.service';
+import { Usuario } from '../../shared/interfaces/usuario.interface';
+import { AuthService } from '../../shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
